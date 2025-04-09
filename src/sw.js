ff374ce090b4bae8e7b0be675a2d2fe793a58e49
96fps <finta96peter@gmail.com>
2025-04-09 01:09:00 -0400

    service worker

---
+++ src/sw.js
@@ -0,0 +1,15 @@
+registerSW();
+function registerSW(){
+  if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('sw.js')
+             .then(function() { alert('Service Worker Registered'); });
+  }                                                                                     }
+function removeSW(){
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+            for (let registration of registrations) {
+              registration.unregister()
+            }
+          });
+  alert("removed");
+}
